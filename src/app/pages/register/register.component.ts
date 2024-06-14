1ce281ee286742e31157c9c95da4643d0b39ac6c
IgorMontezuma20 <igorminranda17@gmail.com>
2024-06-14 20:34:03 -0300

    feat: Tela de Cadastro implementada

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,85 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export default class RegisterComponent {
+export default class RegisterComponent implements OnInit{
 
+  registerForm!: FormGroup;
+  public hide = true;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  // public registerForm: FormGroup = new FormGroup({
+  //   firstName: new FormControl('', Validators.required),
+  //   lastName: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   userName: new FormControl('', Validators.required),
+  //   password: new FormControl('', Validators.required),
+  //   confirmPassword: new FormControl('', Validators.required),
+  // });
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.mustMatch('password', 'confirmPassword')
+    });
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  register() {
+    const credentials = this.registerForm.value;
+    this.verifyPassword();
+
+    console.log(credentials);
+  }
+
+  verifyPassword() {
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+
+    return password === confirmPassword;
+  }
 }
