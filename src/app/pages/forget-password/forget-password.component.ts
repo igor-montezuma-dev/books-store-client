3732171d2846d39a875086b264da18aeb19342eb
IgorMontezuma20 <igorminranda17@gmail.com>
2024-06-16 20:00:13 -0300

    feat: Implementado o envio de email e alteração de senha do usuário

--- src/app/pages/forget-password/forget-password.component.ts
+++ src/app/pages/forget-password/forget-password.component.ts
@@ -1,13 +1,67 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router, RouterModule } from '@angular/router';
+import AuthService from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    RouterModule,
+  ],
   templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.scss']
+  styleUrls: ['./forget-password.component.scss'],
 })
-export default class ForgetPasswordComponent {
+export default class ForgetPasswordComponent implements OnInit {
+  fb = inject(FormBuilder);
+  snackBar = inject(MatSnackBar);
+  router = inject(Router);
+  resetPasswordForm!: FormGroup;
+  public hide = true;
 
+  AuthService = inject(AuthService);
+
+  ngOnInit() {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  submit() {
+    const credentials = this.resetPasswordForm.value;
+    console.log(credentials);
+    this.AuthService.sendEmailService(this.resetPasswordForm.value.email).subscribe({
+      next: (response) => {
+        this.snackBar.open('Email enviado com sucesso!', 'ok', {
+          duration: 3000,
+        });
+        this.router.navigate(['login']);
+        this.resetPasswordForm.reset();
+      },
+      error: (err) => {
+        this.snackBar.open(err.error.message, 'ok', {
+          duration: 3000,
+        });
+        console.error(err);
+      },
+    });
+  }
 }
--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -1,13 +1,86 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import AuthService from 'src/app/services/auth.service';
+import { PasswordValidator } from 'src/app/validators/confirm-password-validator';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    RouterModule,
+  ],
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export default class ResetPasswordComponent {
+  fb = inject(FormBuilder);
+  snackBar = inject(MatSnackBar);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
+  resetPasswordForm!: FormGroup;
 
+  AuthService = inject(AuthService);
+
+  public hide = true;
+  public token!: string;
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      this.token = params['token'];
+      console.log(this.token);
+    });
+    this.resetPasswordForm = this.fb.group(
+      {
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: PasswordValidator('password', 'confirmPassword'),
+      }
+    );
+  }
+
+
+  submit() {
+    const credentials = this.resetPasswordForm.value;
+    let resetOBJ= {
+      password: credentials.password,
+      token: this.token,
+    }
+    console.log(credentials);
+    this.AuthService.resetPasswordService(resetOBJ).subscribe({
+      next: (response) => {
+        this.snackBar.open('Senha atualizada!', 'fechar', {
+          duration: 3000,
+        });
+        this.router.navigate(['login']);
+        this.resetPasswordForm.reset();
+      },
+      error: (err) => {
+        this.snackBar.open(err.error.message, 'fechar', {
+          duration: 3000,
+        });
+        console.error(err);
+      },
+    });
+  }
 }
